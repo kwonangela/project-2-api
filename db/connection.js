fc45251b484708173a2091580c58a74c09e87e48
kwonangela <kwonangelaa@gmail.com>
2023-01-03 01:03:50 -0500

    fixing connection errors

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
 
-// let connectionConfig = {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// };
+let connectionConfig = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
 
 mongoose.set("strictQuery", true);
 
 // mongoose.connect("mongodb://127.0.0.1:27017/GOT-characters", connectionConfig).catch((err) => { console.log(`Error connecting to MongoDB: ${err.message}`)});
 const url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/example'
 
-mongoose.connect(url , mongooseConfig)
+mongoose.connect(url , connectionConfig)
 
 mongoose.connection.on('connected', () => console.log("Connected to database"));
 
